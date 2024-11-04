51e2f2d3d6644964f86b4af79c8967053e7a149d
Samiaboug <bougtoubsamia@gmail.com>
2024-11-04 12:38:07 +0000

    prototpe ORM

--- EloquentORM/prototype/app/Models/User.php
+++ EloquentORM/prototype/app/Models/User.php
@@ -2,10 +2,21 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable ;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable 
 {
+    use HasFactory , Notifiable ;
     protected $fillable = ['firstName' , 'lastName', 'email', 'password'] ;
-    
+    protected $hidden = ['password' , 'remember_token'] ;
+
+    protected function casts():array{
+        return [
+            'email_verified_at' => 'datetime' ,
+            'passwoed' => 'hashed' ,
+        ];
+    }
 }
--- EloquentORM/prototype/database/migrations/2024_10_31_091637_create_user_table.php
+++ EloquentORM/prototype/database/migrations/2024_10_31_091637_create_user_table.php
@@ -14,8 +14,25 @@ return new class extends Migration
             $table->string('firstName');
             $table->string('lastName');
             $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->rememberToken();
             $table->timestamps();
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
         });
     }
 
@@ -23,5 +40,8 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+
     }
 };
