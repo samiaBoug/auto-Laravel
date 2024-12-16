57db35ea9419a71593563d2cf81e173ee72a93f3
Samiaboug <bougtoubsamia@gmail.com>
2024-12-16 16:48:02 +0000

    commit tuto11

---
+++ MaitriseEloquentORM-Avance/tuto11/app/Models/Article.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Category ;
+use App\Models\Tag ;
+
+class Article extends Model
+{
+    //
+    protected $fillable= ['title' , 'content' , 'user_id' , 'category_id'];
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+
+    public function category(){
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tag(){
+        return $this->belongsToMany(Tag::class , 'article_tag');
+    }
+}
