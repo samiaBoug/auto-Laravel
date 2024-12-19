9fba512c54d5d9f1c2e59b2d231d545ed46042fd
Samiaboug <bougtoubsamia@gmail.com>
2024-12-19 15:31:13 +0000

    tuto15

---
+++ MaitriseEloquentORM-Avance/tuto15/resources/views/articles/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>Listes des articles</h1>
+@endsection
---
+++ MaitriseEloquentORM-Avance/tuto15/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth ;
+
+use App\Http\Controllers\DashboardController ;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::middleware([
+    'auth' , 'role:admin'
+])->group(function(){
+    Route::get('/admin', function(){
+        return 'Admin Dashboard' ;
+    });
+});
+Route::get('/settings' , function(){
+    return 'Settings Page ' ;
+})->middleware(['auth' , 'role:admin']);
+
+Route::get('/manager', function(){
+    return 'liste des utilisateur';
+})->middleware('role:manager' , 'auth');
+
+Route::get('/dashboard', function(){
+    return 'permission is working';
+})->middleware('permission:view dashboard');
+
